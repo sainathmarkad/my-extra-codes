ee0dc78c2fb8586377b4e72513dd39c8e0656b98
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-03-03 21:25:07 +0530

    Add files via upload

---
+++ Count_vowels.js
@@ -0,0 +1,52 @@
+
+// Count the number of vowels, present inside nm
+
+
+// Method 1
+
+
+let nm = "revannath";
+
+let count = 0;
+
+for (let i = 0; i < nm.length; i++) {
+
+    if (nm[i] == 'a' || nm[i] == 'e' || nm[i] == 'i' || nm[i] == 'o' || nm[i] == 'u') {
+        count++;
+    }
+}
+console.log(count);
+
+
+// Method 2
+
+
+function count(nm) {
+    let count = 0;
+    for (let i = 0; i < nm.length; i++) {
+        if (nm[i] == 'a' || nm[i] == 'e' || nm[i] == 'i' || nm[i] == 'o' || nm[i] == 'u') {
+            count++;
+        }
+
+    }
+    //  console.log(count);
+    return count;
+}
+console.log(count("revannath"));
+
+
+
+// Method 3
+
+let count = (nm) => {
+    let count = 0;
+    for (let i = 0; i < nm.length; i++) {
+        if (nm[i] == 'a' || nm[i] == 'e' || nm[i] == 'i' || nm[i] == 'o' || nm[i] == 'u') {
+            count++;
+        }
+
+    }
+
+    return count;
+}
+console.log(count("revannath"));
